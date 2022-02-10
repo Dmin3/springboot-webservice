069a0c09a62ae8e50b4724a07f4229a6e705b3e1
Dmin3 <ekdrnzld@gmail.com>
2022-02-10 16:13:02 +0900

    22/2/10

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing 1.JPA Auditing 활성화 2.HelloControllerTest 활성화를 위해 분리
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
